74179092f9dd3b48d08b374c704ac830367c6694
Jyotirmay Bhosale <jyotirmaybhosale0625@gmail.com>
2026-03-20 09:46:39 +0530

    Add program to print even numbers from 1 to 100
    Implement a program to print all even numbers from 1 to 100.

---
+++ forqestion3.cpp
@@ -0,0 +1,13 @@
+// print all even no. from 1 to 100
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i = 1; i <=100; i++)
+    {
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+    }
+    
+
+}
